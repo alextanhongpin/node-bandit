2e8a7bdc81a6dbd362e652ec22b4aceb30209698
alextanhongpin <alextan@seekasia.com>
2017-11-03 07:49:48 +0800

    chore: update thompson sampling

--- bandit/thompson-sampling.js
+++ bandit/thompson-sampling.js
@@ -5,6 +5,8 @@ class ThompsonSampling {
     this.n = n
     this.counts = Array(n).fill(0)
     this.values = Array(n).fill(0)
+    this.a = 1
+    this.b = 1
   }
 
   selectArm () {
@@ -13,7 +15,8 @@ class ThompsonSampling {
       const count = this.counts[i] // Trials
       const value = this.values[i] // Rewards
       const losses = count - value
-      const probability = rBeta(1 + value, 1 + losses)
+      // Draw random sample from beta distribution
+      const probability = rBeta(this.a + value, this.b + losses)
       return probability
     })
     return probabilities.indexOf(Math.max(...probabilities))
@@ -22,21 +25,21 @@ class ThompsonSampling {
   update (chosenArm, reward) {
     this.counts[chosenArm] += 1
     const value = this.values[chosenArm]
-    const newValue = value + reward
-    this.values[chosenArm] = newValue
+    this.values[chosenArm] = value + reward
   }
 }
 
 module.exports = ThompsonSampling
 
-// const thompson = new ThompsonSampling({ n: 3 })
+const thompson = new ThompsonSampling({ n: 3 })
 
-// const arms = [0.1, 0.1, 0.5]
-// arms.forEach((threshold) => {
-//   Array(1000).fill(0).forEach((_, index) => {
-//     const arm = thompson.selectArm()
-//     thompson.update(arm, Math.random() < threshold ? 1 : 0)
-//   })
-// })
+const arms = [0.1, 0.1, 0.5]
+arms.forEach((threshold) => {
+  Array(1000).fill(0).forEach((_, index) => {
+    const arm = thompson.selectArm()
+    console.log(arm)
+    thompson.update(arm, Math.random() < threshold ? 1 : 0)
+  })
+})
 
-// console.log(thompson)
+console.log(thompson)
